1f571fd2f27ed0d9d05e43a602ada998344e89bc
jc <newschapmj1@fastmail.fm>
2025-11-24 10:40:26 +0000

    X

--- crawler/crawler-ssh/src/test/java/fr/pilato/elasticsearch/crawler/fs/crawler/ssh/FileAbstractorSSHTest.java
+++ crawler/crawler-ssh/src/test/java/fr/pilato/elasticsearch/crawler/fs/crawler/ssh/FileAbstractorSSHTest.java
@@ -288,7 +288,7 @@ public class FileAbstractorSSHTest extends AbstractFSCrawlerTestCase {
                 ).containsExactlyInAnyOrder(
                         java.util.stream.Stream.of(values).filter(tuple -> (boolean) tuple.toList().get(1)).<Tuple>map(
                                 tuple -> tuple(tuple.toList().get(0), tuple.toList().get(1), tuple.toList().get(2), tuple.toList().get(3),
-                                        tuple.toList().get(4), tuple.toList().get(5), // isFile, isDirectory, name, extension, path, fullpath
+                                        tuple.toList().get(4), tuple.toList().get(5), // path, fullpath
                                         tuple.toList().get(7), tuple.toList().get(8), tuple.toList().get(9))) // permissions, owner, group
                                 .toArray(Tuple[]::new)
         );
